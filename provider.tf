f0f2e0b177cc25aaa8fc0ad4ce426ccb9dc4ae21
pilarbaquerizo <pilarbaquerizom@gmail.com>
2026-08-10 20:48:09 -0500

    Primer commit

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=5.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  subscription_id = "b497fd69-266c-46a9-b55b-8be0cd579667"
+}
---
+++ resource_group.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "rg-01" {
+  name     = "rg-pbaquerizo-dev-westeurope-001"
+  location = "West Europe"
+}
